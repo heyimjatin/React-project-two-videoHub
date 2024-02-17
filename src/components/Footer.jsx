9a9bbeb47a843b2de36ee9e59832d970a48f7854
Jatin <sharmajatinmr@gmail.com>
2024-02-17 12:55:05 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,81 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineSend } from 'react-icons/ai';
+import { FaInstagram } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p="16" color={'white'}>
+      <Stack direction={['column', 'row']}>
+        <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+          <Heading
+            size="md"
+            textTransform="uppercase"
+            textAlign={['center', 'left']}
+          >
+            Subscribe to get updates
+          </Heading>
+          <HStack borderBottom={'2px solid white'} py={'2'}>
+            <Input
+              placeholder="Enter Email Here..."
+              border={'none'}
+              borderRadius={'none'}
+              outline={'none'}
+              focusBorderColor="none"
+            />
+            <Button
+              p={'0'}
+              colorScheme={'purple'}
+              variant={'ghost'}
+              borderRadius={'0 20px 20px 0'}
+            >
+              <AiOutlineSend size={20} />
+            </Button>
+          </HStack>
+        </VStack>
+
+        <VStack
+          w={'full'}
+          borderLeft={['none', '1px solid white']}
+          borderRight={['none', '1px solid white']}
+        >
+          <Heading textTransform={'uppercase'} textAlign={'center'}>
+            VIDEO HUB
+          </Heading>
+          <Text>All right received</Text>
+        </VStack>
+
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform={'uppercase'}>
+            Social Media
+          </Heading>
+          <Button variant={'link'} color={'whiteAlpha'}>
+            <a
+              target={'blank'}
+              href="https://www.instagram.com/heyimjatin/"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                textTransform: 'uppercase',
+              }}
+            >
+              <FaInstagram size={30} style={{ marginRight: '8px' }} />
+              Instagram
+            </a>
+          </Button>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,104 @@
+import { Box, Heading, Image, Container, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '3xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Hi there! I'm Jatin, a frontend developer passionate about creating
+            engaging web experiences. Based in India, I specialize in React.js,
+            using my skills in HTML, CSS, and JavaScript to turn ideas into
+            functional applications. This project showcases my proficiency in
+            React, featuring slides, services. When I'm not coding, you'll find
+            me editing videos. Explore this project, and feel free to connect
+            for feedback or collaboration!
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h={'full'} w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h={'full'} w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h={'full'} w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h={'full'} w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Meta life is Cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
